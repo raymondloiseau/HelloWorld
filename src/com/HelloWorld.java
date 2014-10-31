07cb199fc428db551236fa81eff04b858a839a2c
ray <ray6568@yahoo.com>
2014-10-31 18:47:25 -0400

    Using Git with eclipse. great

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 			System.out.println("Hello World!");
 			System.out.println("GitHub I am here!");
 			System.out.println("Try out new stuff now!");
+			System.out.println("Try out new stuff now! again");
 	}
 
 }
